bb01a11c0d265e8427c06efedb266edb322f6125
MartinTCode <mtul@pm.me>
2025-04-22 18:17:33 +0200

    Launch JavaFX application using MainApp class in Launcher

--- src/main/java/com/javafullstacklibrary/frontend/Launcher.java
+++ src/main/java/com/javafullstacklibrary/frontend/Launcher.java
@@ -1,7 +1,11 @@
 package com.javafullstacklibrary.frontend;
 
+import com.javafullstacklibrary.MainApp; // Add this import
+import javafx.application.Application;
+
+
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Launcher is working.");
+        Application.launch(MainApp.class, args);
     }
 }
