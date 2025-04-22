bde86675cf507bb7ac6bfbc918fbd1cba3e0ef0d
MartinTCode <mtul@pm.me>
2025-04-22 18:17:47 +0200

    Add StartViewGuestController and FXML for guest view navigation

---
+++ src/main/java/com/javafullstacklibrary/frontend/StartViewGuestController.java
@@ -0,0 +1,22 @@
+// filepath: /home/marty/DATA/SYNCED/_LULEA_STUDIES/COURSES/D0024E, Programutveckling med Java II, Lp4, V25/Work_Group/git/JavaFullStackLibrary/src/main/java/com/javafullstacklibrary/frontend/StartViewGuestController.java
+package com.javafullstacklibrary.frontend;
+
+import javafx.fxml.FXML;
+
+public class StartViewGuestController {
+
+    @FXML
+    private void clickedHomeMenuGuest() {
+        System.out.println("Home menu clicked");
+    }
+
+    @FXML
+    private void clickedSearchMenuGuest() {
+        System.out.println("Search menu clicked");
+    }
+
+    @FXML
+    private void clickedSignInMenuGuest() {
+        System.out.println("Sign-in menu clicked");
+    }
+}
