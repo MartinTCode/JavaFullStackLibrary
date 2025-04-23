954a78c7a8624095661c61eaae78e4861098414c
MartinTCode <mtul@pm.me>
2025-04-23 22:31:55 +0200

    Add SignInUserController to handle user sign-in functionality, with basic boilerplate button reactions for now. Switches back to other view.

---
+++ src/main/java/com/javafullstacklibrary/frontend/SignInUserController.java
@@ -0,0 +1,62 @@
+package com.javafullstacklibrary.frontend;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignInUserController {
+    @FXML
+    private Pane mainPane;
+
+    // access input fields
+    @FXML
+    private TextField ssnField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private void clickedHomeMenuGuest() {
+        try {
+            // Load Start_View_Guest.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javafullstacklibrary/frontend/Start_View_Guest.fxml"));
+            Parent root = loader.load();
+
+
+            // TODO: Reflect on why root.getScene().getWindow() does not work.
+            // Get the current stage from mainPane
+            Stage stage = (Stage) mainPane.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void clickedSearchMenuGuest() {
+        System.out.println("Search menu clicked");
+    }
+
+    @FXML
+    private void clickedSignInButton() {
+        // Print a message indicating the button was clicked
+        System.out.println("Sign-in button clicked");
+
+        // Print the values entered in the text fields
+        String ssn = ssnField.getText();
+        String password = passwordField.getText();
+        System.out.println("SSN: " + ssn);
+        System.out.println("Password: " + password);
+    }
+
+    @FXML
+    private void clickedStaffButton() {
+        System.out.println("Staff button clicked");
+    }
+}
