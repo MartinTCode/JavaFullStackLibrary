0d53a0b3dd833e76096bb78c9aac6dc31518ce13
Eynaan <annaemgr@gmail.com>
2025-04-25 12:53:39 +0200

    Added SignInStaffController and corresponding FXML for staff sign-in

---
+++ src/main/java/com/javafullstacklibrary/frontend/guestControllers/SignInStaffController.java
@@ -0,0 +1,89 @@
+package com.javafullstacklibrary.frontend.guestControllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignInStaffController {
+    @FXML
+    private Pane mainPane;
+
+    // access input fields
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private void clickedHomeMenuGuest() {
+        try {
+            // Load Start_View_Guest.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javafullstacklibrary/frontend/guestViews/Start_View_Guest.fxml"));
+            Parent root = loader.load();
+
+
+            // TODO: Reflect on why root.getScene().getWindow() does not work.
+            // Get the current stage from mainPane
+            Stage stage = (Stage) mainPane.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void clickedSearchMenuGuest() {
+        try {
+            // Load Start_View_Guest.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javafullstacklibrary/frontend/guestViews/Search_Menu_Guest.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage from mainPane
+            Stage stage = (Stage) mainPane.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void clickedSignInButtonAdmin() {
+        // Print a message indicating the button was clicked
+        System.out.println("Admin Sign-in button clicked");
+
+        // Print the values entered in the text fields
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+
+    @FXML
+    private void clickedSignInButtonLibrarian() {
+        // Print a message indicating the button was clicked
+        System.out.println("Librarian Sign-in button clicked");
+
+        // Print the values entered in the text fields
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        System.out.println("SSN: " + username);
+        System.out.println("Password: " + password);
+    }
+
+    @FXML
+    private void clickedfForgotPasswordText() {
+        // Functionality not implemented in current version. 
+    }
+
+    @FXML
+    private void clickedUserButton() {
+        System.out.println("User button clicked");
+    }
+}
