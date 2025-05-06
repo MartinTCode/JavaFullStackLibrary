163739519f7145c6f1eae38568b8e42ee1a58bd6
MartinTCode <mtul@pm.me>
2025-05-06 11:49:24 +0200

    Add Item class with properties and getters/setters for library model, mimics DB model.

---
+++ src/main/java/com/javafullstacklibrary/model/Item.java
@@ -0,0 +1,86 @@
+package com.javafullstacklibrary.model;
+
+
+public class Item {
+
+    private Integer id;
+
+    private String type;
+
+    private String identifier;
+
+    private String identifier2;
+
+    private String title;
+
+    private String publisher;
+
+    private Short ageLimit;
+
+    private String countryOfProduction;
+
+    // Getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier2() {
+        return identifier2;
+    }
+
+    public void setIdentifier2(String identifier2) {
+        this.identifier2 = identifier2;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Short getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(Short ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public String getCountryOfProduction() {
+        return countryOfProduction;
+    }
+
+    public void setCountryOfProduction(String countryOfProduction) {
+        this.countryOfProduction = countryOfProduction;
+    }
+}
