a810c3eff6f550b1354cf94bfee96effca64bb9e
MartinTCode <mtul@pm.me>
2025-05-21 18:05:50 +0200

    REFINE: Simplify logging setup and enhance test structure in StartViewGuestControllerTest by using LoggerUtil and parameterized tests

--- src/test/java/com/javafullstacklibrary/frontend/guestControllers/StartViewGuestControllerTest.java
+++ src/test/java/com/javafullstacklibrary/frontend/guestControllers/StartViewGuestControllerTest.java
@@ -1,18 +1,25 @@
 package com.javafullstacklibrary.frontend.guestControllers;
 
+// Importing necessary classes for JavaFX testing
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Test;
+
+// Importing necessary classes for junit testing
 import org.testfx.framework.junit5.ApplicationTest;
 
+// Importing necessary classes for parameterized tests
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+// Importing necessary classes for assertions
 import static org.testfx.util.NodeQueryUtils.*;
 
-import java.util.logging.FileHandler;
-import java.util.logging.SimpleFormatter;
+// Importing necessary classes for logging
+import com.javafullstacklibrary.utils.LoggerUtil;
 
+// Importing necessary TestFX classes for assertions
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 
@@ -21,20 +28,7 @@ import java.util.logging.Logger;
 
 public class StartViewGuestControllerTest extends ApplicationTest {
 
-    private static final Logger logger = Logger.getLogger(StartViewGuestController.class.getName());
-
-    // Static block to configure logging to a file
-    static {
-        try {
-            FileHandler fh = new FileHandler("StartViewGuestControllerTest.log", true);
-            fh.setFormatter(new SimpleFormatter());
-            fh.setLevel(java.util.logging.Level.ALL);
-            logger.addHandler(fh);
-            logger.setUseParentHandlers(false); // Prevents logging to console
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private static final Logger logger = LoggerUtil.getFileLogger(StartViewGuestControllerTest.class, "StartViewGuestControllerTest.log");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -61,21 +55,52 @@ public class StartViewGuestControllerTest extends ApplicationTest {
         }
     }
 
-    @Test
-    void testClickedSearchMenuGuest() {
-        logger.info("About to click #searchMenuGuest");
-        clickOn("#searchMenuGuest");
-        logger.info("Clicked #searchMenuGuest, checking for #searchField");
+    // Helper to navigate to a view by clicking the menu button
+    private void navigateTo(String buttonId) {
+        clickOn(buttonId);
         sleep(500);
-        verifyThat("#searchField", isVisible());
     }
 
-    @Test
-    void testClickedSignInMenuGuest() {
-        // Simulate clicking the Sign In button
-        clickOn("#signInMenuGuest");
-        // Add assertions here to verify the result of the click
-        sleep(500); // Wait 500ms to allow the new node to load
-        verifyThat("#ssnField", isVisible());
+    // Helper to click a menu button and verify the expected node is visible
+    private void clickMenuAndVerify(String buttonId, String expectedFieldId) {
+        logger.info("Clicking " + buttonId + ", expecting " + expectedFieldId);
+        clickOn(buttonId);
+        sleep(500);
+        verifyThat(expectedFieldId, isVisible());
     }
+
+    @ParameterizedTest
+    @CsvSource({
+        "#searchMenuGuest, #searchField",
+        "#signInMenuGuest, #ssnField"
+    })
+    void testMenuButtonClick_FromStart(String buttonId, String expectedFieldId) {
+        clickMenuAndVerify(buttonId, expectedFieldId);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "#homeMenuGuest, #welcomeBox", 
+        "#signInMenuGuest, #ssnField"
+    })
+    void testMenuButtonClick_FromSearch(String buttonId, String expectedFieldId) {
+        navigateTo("#searchMenuGuest");
+        clickMenuAndVerify(buttonId, expectedFieldId);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "#homeMenuGuest, #welcomeBox",
+        "#searchMenuGuest, #searchField",
+        "#staffButton, #usernameField"
+    })
+    void testMenuButtonClick_FromSignInUser(String buttonId, String expectedFieldId) {
+        navigateTo("#signInMenuGuest");
+        clickMenuAndVerify(buttonId, expectedFieldId);
+    }
+
+    // TODO: try and make it one parameterized test for all buttons menu buttons.
+
+    // Optionally, add a test for Sign_In_Staff as well
+
 }
