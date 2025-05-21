37796759d93862fec1c617cf52d7756c68ed761c
MartinTCode <mtul@pm.me>
2025-05-21 23:43:05 +0200

    Add transitionStep field and constructors to MenuEntry class

--- src/test/java/com/javafullstacklibrary/frontend/guestControllers/MenuEntry.java
+++ src/test/java/com/javafullstacklibrary/frontend/guestControllers/MenuEntry.java
@@ -3,10 +3,25 @@ package com.javafullstacklibrary.frontend.guestControllers;
 public class MenuEntry {
     private final String buttonId;
     private final String fieldId;
+    private final boolean transitionStep;
 
+    // Constructor for MenuEntry
     public MenuEntry(String buttonId, String fieldId) {
         this.buttonId = buttonId;
         this.fieldId = fieldId;
+        this.transitionStep = false;
+    }
+
+    // Overloaded constructor for MenuEntry with transitionStep
+    // This constructor allows you to specify if the entry is a transition step
+    public MenuEntry(String buttonId, String fieldId, boolean transitionStep) {
+        this.buttonId = buttonId;
+        this.fieldId = fieldId;
+        this.transitionStep = transitionStep;
+    }
+
+    public boolean isTransitionStep() {
+        return transitionStep;
     }
 
     public String getButtonId() {
