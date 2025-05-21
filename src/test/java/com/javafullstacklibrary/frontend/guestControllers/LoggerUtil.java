963cfd021810d01bd7adcaf787866d6d5e9a2f73
MartinTCode <mtul@pm.me>
2025-05-21 23:34:51 +0200

    Add TestFX dependencies and implement tests for guest view navigation

---
+++ src/test/java/com/javafullstacklibrary/frontend/guestControllers/LoggerUtil.java
@@ -0,0 +1,22 @@
+package com.javafullstacklibrary.frontend.guestControllers;
+
+import java.util.logging.*;
+
+public class LoggerUtil {
+    public static Logger getFileLogger(Class<?> clazz, String logFileName) {
+        Logger logger = Logger.getLogger(clazz.getName());
+        try {
+            // Prevent duplicate handlers
+            if (logger.getHandlers().length == 0) {
+                FileHandler fh = new FileHandler(logFileName, true);
+                fh.setFormatter(new SimpleFormatter());
+                fh.setLevel(Level.ALL);
+                logger.addHandler(fh);
+                logger.setUseParentHandlers(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return logger;
+    }
+}
