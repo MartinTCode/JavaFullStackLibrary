75f8bb5e639061cc5cdb1bfa9468dd0dec2328b8
Eynaan <annaemgr@gmail.com>
2025-05-27 08:37:34 +0200

    Enhance CreateDvdLibrarianController and CreateJournalLibrarianController: add read input and print data methods

--- src/main/java/com/javafullstacklibrary/frontend/librarianControllers/CreateDvdLibrarianController.java
+++ src/main/java/com/javafullstacklibrary/frontend/librarianControllers/CreateDvdLibrarianController.java
@@ -2,14 +2,24 @@ package com.javafullstacklibrary.frontend.librarianControllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import com.javafullstacklibrary.utils.MenuNavigationHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class CreateDvdLibrarianController {
 
     @FXML
     private BorderPane mainPane;
+
+    // TextFields for form fields
+    @FXML private TextField dvdTitleTextFieldLibrarian;
+    @FXML private TextField dvdImdbcTextFieldLibrarian;
+    @FXML private TextField dvdPublisherTextFieldLibrarian;
+    @FXML private TextField dvdCountryTextFieldLibrarian;
+    @FXML private TextField dvdAgeLimitTextFieldLibrarian;
 
     // ComboBoxes for form fields
     @FXML private ComboBox<String> dvdLanguageComboBoxLibrarian;
@@ -27,6 +37,47 @@ public class CreateDvdLibrarianController {
     @FXML private ComboBox<String> dvdKeywordComboBox2;
     @FXML private ComboBox<String> dvdKeywordComboBox3;
     @FXML private ComboBox<String> dvdKeywordComboBox4;
+
+    /**
+     * This method is called when the controller is initialized.
+     * It calls the applicable methods to initialize the view.
+     */
+    public void initialize() {
+        populateComboBoxes();
+    }
+
+    /**
+     * Populates the ComboBoxes with mock data.
+     * #TODO Replace with actual data retrieval logic.
+     */
+    private void populateComboBoxes() {
+        // Mock data for demonstration
+        ObservableList<String> languages = FXCollections.observableArrayList("English", "Swedish", "German", "French");
+        ObservableList<String> actors = FXCollections.observableArrayList("Actor A", "Actor B", "Actor C", "Actor D");
+        ObservableList<String> directors = FXCollections.observableArrayList("Director X", "Director Y", "Director Z");
+        ObservableList<String> genres = FXCollections.observableArrayList("Action", "Comedy", "Drama", "Documentary");
+        ObservableList<String> keywords = FXCollections.observableArrayList("Keyword1", "Keyword2", "Keyword3", "Keyword4");
+
+        dvdLanguageComboBoxLibrarian.setItems(languages);
+
+        dvdActorComboBox1.setItems(actors);
+        dvdActorComboBox2.setItems(actors);
+        dvdActorComboBox3.setItems(actors);
+        dvdActorComboBox4.setItems(actors);
+
+        dvdDirectorComboBox1.setItems(directors);
+        dvdDirectorComboBox2.setItems(directors);
+        dvdDirectorComboBox3.setItems(directors);
+
+        dvdGenreComboBox1.setItems(genres);
+        dvdGenreComboBox2.setItems(genres);
+        dvdGenreComboBox3.setItems(genres);
+
+        dvdKeywordComboBox1.setItems(keywords);
+        dvdKeywordComboBox2.setItems(keywords);
+        dvdKeywordComboBox3.setItems(keywords);
+        dvdKeywordComboBox4.setItems(keywords);
+    }
 
     // --- Top Menu Navigation Handlers ---
 
@@ -63,14 +114,67 @@ public class CreateDvdLibrarianController {
     // --- Form Button Handlers ---
 
     @FXML
-    private void clickedCancelNewUsersButtonLibrarian(MouseEvent event) {
+    private void clickedCancelNewDvdButtonLibrarian(MouseEvent event) {
         // Go back to Manage Library view
         MenuNavigationHelper.menuClickLibrarian(mainPane, "ManageLibrary");
     }
 
+    /**
+     * Method to handle the click event of the "Save New DVD" button.
+     * This method reads all input from text fields and combo boxes,
+     * and for now prints them to the console.
+     * #TODO Replace with actual save logic to the database or service.
+     */
     @FXML
-    private void clickedSaveNewUsersButtonLibrarian(MouseEvent event) {
-        // Logic to save the new DVD details
-        System.out.println("Save New DVD button clicked");
+    private void clickedSaveNewDvdButtonLibrarian(MouseEvent event) {
+        // Read all input from textfields and comboboxes
+        String title = dvdTitleTextFieldLibrarian.getText();
+        String imdbc = dvdImdbcTextFieldLibrarian.getText();
+        String publisher = dvdPublisherTextFieldLibrarian.getText();
+        String country = dvdCountryTextFieldLibrarian.getText();
+        String ageLimit = dvdAgeLimitTextFieldLibrarian.getText();
+
+        String language = dvdLanguageComboBoxLibrarian.getValue();
+
+        String actor1 = dvdActorComboBox1.getValue();
+        String actor2 = dvdActorComboBox2.getValue();
+        String actor3 = dvdActorComboBox3.getValue();
+        String actor4 = dvdActorComboBox4.getValue();
+
+        String director1 = dvdDirectorComboBox1.getValue();
+        String director2 = dvdDirectorComboBox2.getValue();
+        String director3 = dvdDirectorComboBox3.getValue();
+
+        String genre1 = dvdGenreComboBox1.getValue();
+        String genre2 = dvdGenreComboBox2.getValue();
+        String genre3 = dvdGenreComboBox3.getValue();
+
+        String keyword1 = dvdKeywordComboBox1.getValue();
+        String keyword2 = dvdKeywordComboBox2.getValue();
+        String keyword3 = dvdKeywordComboBox3.getValue();
+        String keyword4 = dvdKeywordComboBox4.getValue();
+
+        // Print all values to the console, switch this out to actual save logic later
+        System.out.println("Save New DVD button clicked. Input values:");
+        System.out.println("Title: " + title);
+        System.out.println("IMDBC: " + imdbc);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Country: " + country);
+        System.out.println("Age Limit: " + ageLimit);
+        System.out.println("Language: " + language);
+        System.out.println("Actor 1: " + actor1);
+        System.out.println("Actor 2: " + actor2);
+        System.out.println("Actor 3: " + actor3);
+        System.out.println("Actor 4: " + actor4);
+        System.out.println("Director 1: " + director1);
+        System.out.println("Director 2: " + director2);
+        System.out.println("Director 3: " + director3);
+        System.out.println("Genre 1: " + genre1);
+        System.out.println("Genre 2: " + genre2);
+        System.out.println("Genre 3: " + genre3);
+        System.out.println("Keyword 1: " + keyword1);
+        System.out.println("Keyword 2: " + keyword2);
+        System.out.println("Keyword 3: " + keyword3);
+        System.out.println("Keyword 4: " + keyword4);
     }
 }
--- src/main/java/com/javafullstacklibrary/frontend/librarianControllers/CreateJournalLibrarianController.java
+++ src/main/java/com/javafullstacklibrary/frontend/librarianControllers/CreateJournalLibrarianController.java
@@ -2,14 +2,22 @@ package com.javafullstacklibrary.frontend.librarianControllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import com.javafullstacklibrary.utils.MenuNavigationHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class CreateJournalLibrarianController {
 
     @FXML
     private BorderPane mainPane;
+
+    // TextFields for form fields
+    @FXML private TextField journalTitleTextFieldLibrarian;
+    @FXML private TextField journalIssnTextFieldLibrarian;
+    @FXML private TextField journalPublisherTextFieldLibrarian;
 
     // ComboBoxes for form fields
     @FXML private ComboBox<String> journalLanguageComboBoxLibrarian;
@@ -62,14 +70,98 @@ public class CreateJournalLibrarianController {
     // --- Form Button Handlers ---
 
     @FXML
-    private void clickedCancelNewBookButtonLibrarian(MouseEvent event) {
+    private void clickedCancelNewJournalButtonLibrarian(MouseEvent event) {
         // Go back to Manage Library view
         MenuNavigationHelper.menuClickLibrarian(mainPane, "ManageLibrary");
     }
 
+    /**
+     * Handler for the "Save New Journal" button.
+     * Reads input from the form fields and currently prints them to the console.
+     * #TODO Replace with actual save logic later.
+     * @param event
+     */
     @FXML
-    private void clickedSaveNewBookButtonLibrarian(MouseEvent event) {
-        // Logic to save the new journal entry
-        System.out.println("Clicked Save New Journal Button");
+    private void clickedSaveNewJournalButtonLibrarian(MouseEvent event) {
+        // Read all input from textfields and comboboxes
+        String title = journalTitleTextFieldLibrarian.getText();
+        String issn = journalIssnTextFieldLibrarian.getText();
+        String publisher = journalPublisherTextFieldLibrarian.getText();
+
+        String language = journalLanguageComboBoxLibrarian.getValue();
+        String floor = journalFloorComboBoxLibrarian.getValue();
+        String section = journalSectionComboBoxLibrarian.getValue();
+        String shelf = journalShelfComboBoxLibrarian.getValue();
+        String position = journalPositionComboBoxLibrarian.getValue();
+
+        String keyword1 = journalKeywordComboBoxLibrarian1.getValue();
+        String keyword2 = journalKeywordComboBoxLibrarian2.getValue();
+        String keyword3 = journalKeywordComboBoxLibrarian3.getValue();
+
+        String author1 = journalAuthorComboBoxLibrarian1.getValue();
+        String author2 = journalAuthorComboBoxLibrarian2.getValue();
+        String author3 = journalAuthorComboBoxLibrarian3.getValue();
+        String author4 = journalAuthorComboBoxLibrarian4.getValue();
+        String author5 = journalAuthorComboBoxLibrarian5.getValue();
+
+        // Print all values to the console, changed to actual save logic later
+        System.out.println("Save New Journal button clicked. Input values:");
+        System.out.println("Title: " + title);
+        System.out.println("ISSN: " + issn);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Language: " + language);
+        System.out.println("Floor: " + floor);
+        System.out.println("Section: " + section);
+        System.out.println("Shelf: " + shelf);
+        System.out.println("Position: " + position);
+        System.out.println("Keyword 1: " + keyword1);
+        System.out.println("Keyword 2: " + keyword2);
+        System.out.println("Keyword 3: " + keyword3);
+        System.out.println("Author 1: " + author1);
+        System.out.println("Author 2: " + author2);
+        System.out.println("Author 3: " + author3);
+        System.out.println("Author 4: " + author4);
+        System.out.println("Author 5: " + author5);
+    }
+
+    /**
+     * Initializes the controller and calls applicable methods
+     */
+    @FXML
+    public void initialize() {
+        populateComboBoxes();
+    }
+
+    /**
+     * Populates the combo boxes with mock data.
+     * This method is called during the initialization of the controller.
+     * #TODO Change this to fetch actual data from the database when implemented
+     */
+    private void populateComboBoxes() {
+        // Mock data for demonstration, change to fetch actual data later
+        ObservableList<String> languages = FXCollections.observableArrayList("English", "Swedish", "German", "French");
+        ObservableList<String> floors = FXCollections.observableArrayList("1", "2", "3", "4");
+        ObservableList<String> sections = FXCollections.observableArrayList("A", "B", "C", "D");
+        ObservableList<String> shelves = FXCollections.observableArrayList("Shelf 1", "Shelf 2", "Shelf 3");
+        ObservableList<String> positions = FXCollections.observableArrayList("1", "2", "3", "4", "5");
+        ObservableList<String> keywords = FXCollections.observableArrayList("Keyword1", "Keyword2", "Keyword3", "Keyword4");
+        ObservableList<String> authors = FXCollections.observableArrayList("Author A", "Author B", "Author C", "Author D", "Author E");
+
+        // Set the items for each ComboBox
+        journalLanguageComboBoxLibrarian.setItems(languages);
+        journalFloorComboBoxLibrarian.setItems(floors);
+        journalSectionComboBoxLibrarian.setItems(sections);
+        journalShelfComboBoxLibrarian.setItems(shelves);
+        journalPositionComboBoxLibrarian.setItems(positions);
+
+        journalKeywordComboBoxLibrarian1.setItems(keywords);
+        journalKeywordComboBoxLibrarian2.setItems(keywords);
+        journalKeywordComboBoxLibrarian3.setItems(keywords);
+
+        journalAuthorComboBoxLibrarian1.setItems(authors);
+        journalAuthorComboBoxLibrarian2.setItems(authors);
+        journalAuthorComboBoxLibrarian3.setItems(authors);
+        journalAuthorComboBoxLibrarian4.setItems(authors);
+        journalAuthorComboBoxLibrarian5.setItems(authors);
     }
 }
